6a69c470380aca0e3ca1cfc2051116364c4bba2c
Seungwoo Oh <sw6016@hanmail.net>
2023-04-22 14:32:26 +0900

    commit

--- SpringMVC02/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC02/src/main/java/kr/board/controller/BoardController.java
@@ -20,24 +20,12 @@ import kr.board.mapper.BoardMapper;
 @Controller
 public class BoardController {
 	
-	@Autowired
-	BoardMapper boardMapper;
+
 	
 	@RequestMapping("/")
 	public String main() {
 		return "main";
 	}
 	
-	// @ResponseBody  -> jackson-databind(객체를 JSON 데이터포맷으로 변환)
-	@RequestMapping("/boardList.do")
-	public @ResponseBody List<Board> boardList(){
-		List<Board> list = boardMapper.getLists();
-		return list; // JSON 데이터 형식으로 변환(API)해서 리턴(응답)하겠다.
-	}
-	
-	@RequestMapping("/boardInsert.do")
-	 public @ResponseBody void boardInsert(Board vo) {
-		boardMapper.boardInsert(vo); // 등록 성공
-	}
 	
 }
